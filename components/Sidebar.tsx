741f5fcd554810c653ae5c85f47645cf8c0ad9bb
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-07-30 21:34:54 -0700

    sidebar tailwind styles finished

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,19 +10,25 @@ const Sidebar = () => {
       <img
         src="https://avatars.githubusercontent.com/u/117542307?s=400&u=fb80fe048c7d3925b6aad3c1b57bbc6248d45923&v=4"
         alt="user avatar"
+        className="w-32 h-32 mx-auto rounded-full"
       />
-      <h1>
+      <h3 className="my-4 text-3xl font-medium tracking-wider">
         <span>Otabek</span>
         Sodiqov
-      </h1>
-      <p>Web Developer</p>
-      <p>
+      </h3>
+      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">Web Developer</p>
+      <a
+        href=""
+        download="name"
+        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
+      >
         <GiTie className="w-6 h-6" />
         Download Resume
-      </p>
-      <div>
+      </a>
+      {/* social media */}
+      <div className="flex justify-around w-9/12 mx-auto my-5 text-gray-500 md:w-full">
         <a href="">
-          <AiFillGithub className="w-8 h-8" />
+          <AiFillGithub className="w-8 h-8 cursor-pointer" />
         </a>
         <a href="">
           <AiFillYoutube className="w-8 h-8" />
@@ -31,19 +37,24 @@ const Sidebar = () => {
           <AiFillLinkedin className="w-8 h-8" />
         </a>
       </div>
-      <div>
-        <div>
+      {/* address */}
+      <div
+        className="py-4 my-5 bg-gray-200"
+        style={{ marginLeft: "-1rem", marginRight: "-1rem" }}
+      >
+        <div className="flex items-center justify-center gap-1">
           <GoLocation />
           <span>Bukhara,Uzbekistan</span>
         </div>
-        <p>sodikovotabeksobirjonovich1509@gmail.com</p>
-        <p>
-          <LuPhoneCall />
-          +998936843757 / +998934506315
-        </p>
+        <p className="my-2">sodikovotabeksobirjonovich1509@gmail.com</p>
+        <p className="my-2">+998936843757 / +998934506315</p>
       </div>
-      <button>Email me</button>
-      <button>Toggle theme</button>
+      <button className="w-8/12 px-5 py-2 my-2 rounded-full bg-gradient-to-r from-green-400 to-blue-400">
+        Email me
+      </button>
+      <button className="w-8/12 px-5 py-2 my-2 rounded-full bg-gradient-to-r from-green-400 to-blue-400">
+        Toggle theme
+      </button>
     </div>
   );
 };
