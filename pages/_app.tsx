dd34fdd6beb1ac8639fbbf55efdbc841d409b572
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-07-30 22:06:38 -0700

    padding fixed for md screens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14">
+    <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
       <div className="col-span-12 px-4 text-center bg-white lg:col-span-3 rounded-2xl">
         <Sidebar />
       </div>
