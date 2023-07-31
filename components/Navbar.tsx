ab535d7ad5c2ce7e0e5d65c86bd345737da0bf23
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-07-30 23:43:25 -0700

    NavItem component created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,53 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { FunctionComponent, useEffect, useState } from "react";
+
+const NavItem: FunctionComponent<{
+  activeItem: string;
+  setActiveItem: Function;
+  name: string;
+  route: string;
+}> = ({ activeItem, setActiveItem, name, route }) => {
+  return activeItem !== name ? (
+    <Link href={route}>
+      <span onClick={() => setActiveItem(name)}>About</span>
+    </Link>
+  ) : null;
+};
 
 const Navbar = () => {
-  const [activeItem, setActiveItem] = useState("");
+  const [activeItem, setActiveItem] = useState("About");
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    if (pathname === "/") setActiveItem("About");
+    if (pathname === "/projects") setActiveItem("Projects");
+    if (pathname === "/resume") setActiveItem("Resume");
+
+    // eslint-disable-next-line
+  }, []);
   return (
     <div>
-      <span>{activeItem}</span>
-      <div className="text-red font-lg">
-        {activeItem !== "About" && (
-          <Link href="/">
-            <span onClick={() => setActiveItem("About")}>About</span>
-          </Link>
-        )}
-        {activeItem !== "Resume" && (
-          <Link href="/resume">
-            <span onClick={() => setActiveItem("Resume")}>Resume</span>
-          </Link>
-        )}
-        {activeItem !== "Projects" && (
-          <Link href="/projects">
-            <span onClick={() => setActiveItem("Projects")}>Projects</span>
-          </Link>
-        )}
+      <span className="text-green">{activeItem}</span>
+      <div className="flex space-x-3 text-red-500 font-lg">
+        <NavItem
+          activeItem="About"
+          name="About"
+          route="/"
+          setActiveItem={() => setActiveItem}
+        />
+        <NavItem
+          activeItem="Projects"
+          name="Projects"
+          route="/projects"
+          setActiveItem={() => setActiveItem}
+        />
+        <NavItem
+          activeItem="Resume"
+          name="Resume"
+          route="/resume"
+          setActiveItem={() => setActiveItem}
+        />
       </div>
     </div>
   );
