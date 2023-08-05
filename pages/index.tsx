670fecbb44cc7d0852ebe1c9765c9f0a6fcb6b3f
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-05 04:50:25 -0700

    about page layout finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
-const index = () =>  {
+import ServiceCard from "../components/ServiceCard";
+import { services } from "../data";
+
+const index = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h5>
+        I am currently pursuing B.Tech Degree(Final Year) in Computer Science
+        Engineering from Academy of Technology. I have 3+ years of experience in
+        Web Development and i have a Youtube Channel where i teach Full Stack
+        Web Development Projects
+      </h5>
+      <div>
+        <h6>What I Offer</h6>
+        <div>
+          {services.map((service) => (
+            <ServiceCard key="1" service={service} />
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default index;
