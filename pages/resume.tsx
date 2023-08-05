0a2c2c35e2bd2d98dedfaa059daec91ffca049db
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-05 06:47:27 -0700

    resume page finished

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,7 +1,52 @@
+import Bar from "../components/Bar";
+import { languages, tools } from "../data";
+
 const resume = () => {
   return (
-    <div className="py-4">
-      <h1>Resume Page</h1>
+    <div className="px-6 py-2">
+      {/* education and experience */}
+      <div className="grid gap-6 md:grid-cols-2">
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Education</h5>
+          <div>
+            <h5 className="my-2 text-xl font-bold">
+              Computer Science Engineering
+            </h5>
+            <p className="font-semibold">Academy of Technology(2017-2021)</p>
+            <p className="my-3">
+              I am currently pursuing B.tech in Computer Science Engineering
+              from Academy of Technology
+            </p>
+          </div>
+        </div>
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Experience</h5>
+          <div>
+            <h5 className="my-2 text-xl font-bold">Software Engineer Jr</h5>
+            <p className="font-semibold">TCS(2021 - on)</p>
+            <p className="my-3">I do not know why i am doing this job</p>
+          </div>
+        </div>
+      </div>
+      {/* languages and tools */}
+      <div className="grid gap-6 md:grid-cols-2">
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Languages & Frameworks</h5>
+          <div className="my-2">
+            {languages.map((lang) => (
+              <Bar data={lang} key={lang.name} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Tools & Softwares</h5>
+          <div className="my-2">
+            {tools.map((tool) => (
+              <Bar data={tool} key={tool.name} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
