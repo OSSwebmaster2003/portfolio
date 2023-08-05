5bfb86d1d7e1c98f1a205adda9cb64bc14170da5
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-05 06:17:10 -0700

    about page design finished

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,23 +1,19 @@
 import { FunctionComponent } from "react";
 import { IService } from "../type";
-// import { Service } from "../types";
-// import { motion } from 'framer-motion'
 
 const ServiceCard: FunctionComponent<{ service: IService }> = ({
-  service: { Icon, title, about },
+  service: { Icon, about, title },
 }) => {
-  //XSS attack :( on our portfolio btw, as an alternate use npm i dom purify
-  function createMarkup() {
+  const createMarkup = () => {
     return {
       __html: about,
     };
-  }
-
+  };
   return (
-    <div className="flex items-center p-2 space-x-4 ">
+    <div className="flex items-center p-2 space-x-2">
       <Icon className="w-12 h-12 text-green" />
-      <div className="">
-        <h6 className="font-bold">{title}</h6>
+      <div>
+        <h4 className="font-bold">{title}</h4>
         <p dangerouslySetInnerHTML={createMarkup()} />
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,26 @@ import { services } from "../data";
 
 const index = () => {
   return (
-    <div>
-      <h5>
+    <div className="flex flex-col flex-grow px-6 pt-1">
+      <h5 className="my-3 text-base font-medium">
         I am currently pursuing B.Tech Degree(Final Year) in Computer Science
         Engineering from Academy of Technology. I have 3+ years of experience in
         Web Development and i have a Youtube Channel where i teach Full Stack
         Web Development Projects
       </h5>
-      <div>
-        <h6>What I Offer</h6>
-        <div>
+      <div
+        className="flex-grow p-4 mt-5 bg-gray-400"
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h6 className="my-3 text-xl font-bold tracking-wide">What I Offer</h6>
+        <div className="grid gap-6 my-3 md:grid-cols-2">
           {services.map((service) => (
-            <ServiceCard key="1" service={service} />
+            <div
+              className="bg-gray-200 rounded lg:col-span-1"
+              key={service.title}
+            >
+              <ServiceCard service={service} />
+            </div>
           ))}
         </div>
       </div>
