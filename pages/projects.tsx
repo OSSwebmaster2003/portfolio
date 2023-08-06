121cfdc2c1763a0a5f816f4998cf65f7a5dc4b3a
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-06 02:01:27 -0700

    projects page body design finished

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,12 +3,12 @@ import { projects } from "../data";
 
 const Projects = () => {
   return (
-    <div className="py-4">
+    <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
       <nav>Navbar</nav>
       <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
           <div
-            className="col-span-12 p-2 sm:col-span-6 lg:col-span-4"
+            className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-700"
             key={project.name}
           >
             <ProjectCard project={project} />
