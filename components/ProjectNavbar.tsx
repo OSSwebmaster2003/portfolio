581da35bab5dc3ac1cf150c2e25cb99de84021f2
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-06 02:10:38 -0700

    projects navbar design finished

--- components/ProjectNavbar.tsx
+++ components/ProjectNavbar.tsx
@@ -4,12 +4,14 @@ import { Category } from "../type";
 export const NavItem: FunctionComponent<{ value: Category | "all" }> = ({
   value,
 }) => {
-  return <li>{value}</li>;
+  return (
+    <li className="capitalize cursor-pointer hover:text-green">{value}</li>
+  );
 };
 
 const ProjectNavbar = () => {
   return (
-    <div>
+    <div className="flex px-3 py-2 space-x-3 overflow-x-auto list-none">
       <NavItem value="all" />
       <NavItem value="react" />
       <NavItem value="mongo" />
