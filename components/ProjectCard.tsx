be263deca5e2692ef2a84a862051dd8c5b5fc655
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-06 23:12:33 -0700

    scrollbar changed

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -40,9 +40,12 @@ const ProjectCard: FunctionComponent<{
       <p className="my-2 text-center">{name}</p>
 
       {showDetail === id && (
-        <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 md:grid-cols-2 gap-x-12 dark:text-white dark:bg-dark-100">
+        <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 rounded-lg md:p-10 md:grid-cols-2 gap-x-12 dark:text-white dark:bg-dark-100">
           <motion.div variants={stagger} initial="initial" animate="animate">
-            <motion.div variants={fadeInUp}>
+            <motion.div
+              variants={fadeInUp}
+              className="border-4 border-gray-100"
+            >
               <Image
                 src={image_path}
                 alt={name}
