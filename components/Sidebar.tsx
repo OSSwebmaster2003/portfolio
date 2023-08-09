abab6498450632967f40dbca46101059f1ef2465
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-09 01:29:51 -0700

    resume uploaded to the project

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -29,8 +29,8 @@ const Sidebar = () => {
         Web Developer
       </p>
       <a
-        href="/images/blink.jpg"
-        download="Otabek Sodiqov Resume"
+        href="/resume/OtabekSodiqov.pdf"
+        download="OtabekSodiqov"
         className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200"
       >
         <GiTie className="w-6 h-6" />
