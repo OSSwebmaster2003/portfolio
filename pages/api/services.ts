4e6c454eaf758b940c2d5836c790232df38fac9b
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-08 23:40:55 -0700

    service api changed

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,8 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { services } from "../../data";
-
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  // by default get request
+  //BY default get request
 
-  res.status(200).json(services);
+  //! complex backend logic :(
+  console.log("API", services);
+
+  res.status(200).json({ services });
 };
