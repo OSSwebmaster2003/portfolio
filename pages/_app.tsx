6cee14b60901c742ea4124d9f34e2de5dd09bf89
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-09 02:10:43 -0700

    some paddings changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute="class">
-      <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
+      <div className="grid grid-cols-12 gap-6 px-5 xl:px-36 lg:px-8 my-14 sm:px-20 md:px-24">
         <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
           <Sidebar />
         </div>
