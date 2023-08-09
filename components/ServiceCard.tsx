342395ba29795c1577ccb540a63b9ed3fc4635b7
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-08-09 02:23:05 -0700

    some design errors fixed

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -10,7 +10,7 @@ const ServiceCard: FunctionComponent<{ service: IService }> = ({
     };
   };
   return (
-    <div className="flex items-center p-2 space-x-2">
+    <div className="flex items-center p-3 space-x-2 lg:px-5 lg:py-4">
       <Icon className="w-12 h-12 text-green" />
       <div>
         <h4 className="font-bold">{title}</h4>
